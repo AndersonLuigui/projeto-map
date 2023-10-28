2a8c66868b586e4679a4a8e2f49d4098719d52dd
ANDERSON LUIGUI <alns1536@hotmail.com>
2023-10-27 21:33:56 -0300

    editando site

--- js/script.js
+++ js/script.js
@@ -2,7 +2,20 @@ function mostra(el) {
     let display = document.getElementById(el).style.display;
     if(display == "block") {
         document.getElementById(el).style.display = 'none';
+        document.getElementById(el).style.transition = '1s';
+
     } else {
         document.getElementById(el).style.display = 'block';
+        document.getElementById(el).style.transition = '1s';
     }
 }
+
+const tela = document.getElementById('tela');
+const regioes = document.getElementById('regioes');
+
+tela.addEventListener('click', function(e) {
+    if (e === tela.sergipe) {
+        regioes.scrollIntoView({ behavior: 'smooth' });
+    }
+});
+
